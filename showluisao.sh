56624f6aa9ac52d0a116ef93c9d9228ae6dca852
luis.araujo <luis.araujo-pit@parceiros-kroton.com.br>
2018-03-27 10:09:35 -0300

    deploy sh

---
+++ showluisao.sh
@@ -0,0 +1,6 @@
+echo "# show_luisao" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/solutionslga/show_luisao.git
+git push -u origin master
